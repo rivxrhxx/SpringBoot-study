3b2e0c120628731baf2b696be113e02f21d3bd8e
Kanghee <kh63167574@gmail.com>
2025-09-09 19:12:18 +0900

    feat: 회원레포지토리와 도메인 만들기

--- SpringBootPrac/src/main/java/com/example/springbootprac/repository/MemberRepository.java
+++ SpringBootPrac/src/main/java/com/example/springbootprac/repository/MemberRepository.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
+    Member save(Member member); //회원저장
     Optional<Member> findById(Long id);
-    Optional<Member> findByEmail(String name);
-    List<Member> findAll();
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); //지금까지 저장된 모든회원 반환
 }
